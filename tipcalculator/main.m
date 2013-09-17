e4d4c7b69f7ff2e43ff9944affa3b3ff8c2e5f71
Tony Wu <twu@twitter.com>
2013-09-17 16:51:15 -0700

    Initial Commit

---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Tony Wu on 9/17/13.
+//  Copyright (c) 2013 Tony Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
