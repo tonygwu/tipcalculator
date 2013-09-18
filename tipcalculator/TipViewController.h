a7c032077b3cbbc8be7a59b37cb79d371210b378
Tony Wu <twu@twitter.com>
2013-09-17 17:47:03 -0700

    Implemented tipcalculator from tutorial video.

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Tony Wu on 9/17/13.
+//  Copyright (c) 2013 Tony Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
